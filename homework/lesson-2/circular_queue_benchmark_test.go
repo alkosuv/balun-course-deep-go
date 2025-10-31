4d7e55afc0a628838cb4e7de0b8c0470c67ae030
alkosuv <al.ko.suv@gmail.com>
2025-10-31 09:42:33 +0300

    Update circular_queue_benchmark_test.go

--- homework/lesson-2/circular_queue_benchmark_test.go
+++ homework/lesson-2/circular_queue_benchmark_test.go
@@ -23,3 +23,163 @@ func BenchmarkCircularQueue_PushPop(b *testing.B) {
 		queue.Pop()
 	}
 }
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueueWithoutLen_PushPop_10_000(b *testing.B) {
+	queue := NewCircularQueueWithoutLen[int](b.N)
+	size := 10_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueue_PushPop_10_000(b *testing.B) {
+	queue := NewCircularQueue[int](b.N)
+	size := 10_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueueWithoutLen_PushPop_100_000(b *testing.B) {
+	queue := NewCircularQueueWithoutLen[int](b.N)
+	size := 100_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueue_PushPop_100_000(b *testing.B) {
+	queue := NewCircularQueue[int](b.N)
+	size := 100_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueueWithoutLen_PushPop_1_000_000(b *testing.B) {
+	queue := NewCircularQueueWithoutLen[int](b.N)
+	size := 1_000_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueue_PushPop_1_000_000(b *testing.B) {
+	queue := NewCircularQueue[int](b.N)
+	size := 1_000_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueueWithoutLen_PushPop_1_000_000_000(b *testing.B) {
+	queue := NewCircularQueueWithoutLen[int](b.N)
+	size := 1_000_000_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
+
+// Benchmark для последовательных операций Push/Pop
+func BenchmarkCircularQueue_PushPop_1_000_000_000(b *testing.B) {
+	queue := NewCircularQueue[int](b.N)
+	size := 1_000_000_000
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		// queue.Push(i)
+		// queue.Pop()
+
+		for j := 0; j <= size; j++ {
+			queue.Push(j)
+		}
+
+		for j := 0; j <= size; j++ {
+			queue.Pop()
+		}
+	}
+}
