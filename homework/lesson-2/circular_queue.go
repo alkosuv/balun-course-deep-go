98699e14b2887fcf9d6361a8ace887e698f0d8e3
alkosuv <al.ko.suv@gmail.com>
2025-10-31 08:15:19 +0300

    Add homewoek/lesson-2

---
+++ homework/lesson-2/circular_queue.go
@@ -0,0 +1,82 @@
+package main
+
+type CircularQueue[T int8|int16|int32|int64|int] struct {
+    queue []T
+    head  int
+    tail  int
+    len   int
+    cap   int
+}
+
+// NewCircularQueue создать очередь с определенным размером буффера
+func NewCircularQueue[T int8|int16|int32|int64|int](size int) CircularQueue[T] {
+    return CircularQueue[T]{
+        queue: make([]T, size),
+        cap:   size,
+        len:   0,
+        head:  0,
+        tail:  0,
+    }
+}
+
+// Dequeue используется для извлечения и одновременного удаления значения из начала очереди
+func (q *CircularQueue[T]) Dequeue() (T, bool) {
+    return q.Front(), q.Pop()
+}
+
+// Push добавить значение в конец очереди (false, если очередь заполнена)
+func (q *CircularQueue[T]) Push(value T) bool {
+    if q.Full() {
+        return false
+    }
+
+    q.queue[q.tail] = value
+    q.tail = (q.tail + 1) % q.cap
+    q.len++
+    return true
+}
+
+// Pop удалить значение из начала очереди (false, если очередь пустая)
+func (q *CircularQueue[T]) Pop() bool {
+    if q.Empty() {
+        return false
+    }
+
+    q.head = (q.head + 1) % q.cap
+    q.len--
+    return true
+}
+
+// Front получить значение из начала очереди (-1, если очередь пустая)
+func (q *CircularQueue[T]) Front() T {
+    if q.Empty() {
+        return -1
+    }
+    return q.queue[q.head]
+}
+
+// Back получить значение из конца очереди (-1, если очередь пустая)
+func (q *CircularQueue[T]) Back() T {
+    if q.Empty() {
+        return -1
+    }
+
+    // Вычисляем позицию последнего элемента
+    tail := (q.tail - 1 + q.cap) % q.cap
+    return q.queue[tail]
+}
+
+// Empty проверить пустая ли очередь
+func (q *CircularQueue[T]) Empty() bool {
+    return q.len == 0
+}
+
+// Full проверить заполнена ли очередь
+func (q *CircularQueue[T]) Full() bool {
+    return q.len == q.cap
+}
+
+// getQueue получить очередь
+func (q *CircularQueue[T]) getQueue() []T {
+    return q.queue
+}
---
+++ homework/lesson-2/circular_queue_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func TestCircularQueue(t *testing.T) {
+	const queueSize = 3
+	queue := NewCircularQueue[int](queueSize)
+
+	assert.True(t, queue.Empty())
+	assert.False(t, queue.Full())
+
+	assert.Equal(t, -1, queue.Front())
+	assert.Equal(t, -1, queue.Back())
+	assert.False(t, queue.Pop())
+
+	assert.True(t, queue.Push(1))
+	assert.True(t, queue.Push(2))
+	assert.True(t, queue.Push(3))
+	assert.False(t, queue.Push(4))
+
+	assert.True(t, reflect.DeepEqual([]int{1, 2, 3}, queue.getQueue()))
+
+	assert.False(t, queue.Empty())
+	assert.True(t, queue.Full())
+
+	assert.Equal(t, 1, queue.Front())
+	assert.Equal(t, 3, queue.Back())
+
+	assert.True(t, queue.Pop())
+	assert.False(t, queue.Empty())
+	assert.False(t, queue.Full())
+	assert.True(t, queue.Push(4))
+
+	assert.True(t, reflect.DeepEqual([]int{4, 2, 3}, queue.getQueue()))
+
+	assert.Equal(t, 2, queue.Front())
+	assert.Equal(t, 4, queue.Back())
+
+	value, ok := queue.Dequeue()
+	assert.Equal(t, 2 ,value)
+	assert.Equal(t, true, ok)
+
+	value, ok = queue.Dequeue()
+	assert.Equal(t, 3 ,value)
+	assert.Equal(t, true, ok)
+
+	value, ok = queue.Dequeue()
+	assert.Equal(t, 4 ,value)
+	assert.Equal(t, true, ok)
+
+	value, ok = queue.Dequeue()
+	assert.Equal(t, -1 ,value)
+	assert.Equal(t, false, ok)
+
+	assert.False(t, queue.Pop())
+
+	assert.True(t, queue.Empty())
+	assert.False(t, queue.Full())
+}
